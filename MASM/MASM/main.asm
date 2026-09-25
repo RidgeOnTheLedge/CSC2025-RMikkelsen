e68bf2846a8643cc158c2f33a4ffc39b85f20f92
RidgeOnTheLedge <ridgem970@gmail.com>
2026-09-25 16:19:40 -0600

    Created Hello World! project

--- MASM/MASM/main.asm
+++ MASM/MASM/main.asm
@@ -2,18 +2,35 @@
 .model flat
 
 extern _ExitProcess@4:near
+extern _GetStdHandle@4:near
+extern _WriteConsoleA@20:near
 
 .data
+
+	msg			DB	'Hello World!', 0
+	outhandle	DD	?
+	written		DD	?
 
 
 .code
 
-main PROC near
+main PROC near	
+	           
+	push	-11 ; STD_OUTPUT_HANDLE
+	call	_GetStdHandle@4
+	mov		outhandle, eax
+	
+	; WriteConsole(handle, &msg[0], 13, &written,0)	 
+	push	0
+	push	offset written
+	push	12
+	push	offset msg
+	push	outhandle
+	call	_WriteConsoleA@20
 
 	push	0
 	call	_ExitProcess@4
-
+	 
 main ENDP
-
-
+ 
 END 
